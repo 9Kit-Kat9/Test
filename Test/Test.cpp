f3fa102a0188c2698d9c3923ec4c8f915a105dc8
nikillar74 <nikillar74@gmail.com>
2025-05-16 11:58:45 +0300

    Добавление тестовых примеров

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -5,7 +5,6 @@ using namespace std;
 struct ListNode {
     int val;
     ListNode* next;
-    ListNode(int x) : val(x), next(nullptr) {}
 };
 
 // Функция для вывода списка
@@ -25,14 +24,18 @@ ListNode* createList(const int* arr, int size) {
     ListNode* head = new ListNode(arr[0]);
     ListNode* current = head;
     for (int i = 1; i < size; ++i) {
-        current->next = new ListNode(arr[i]);
-        current = current->next;
+        ListNode<T>* newNode = new ListNode<T>;
+        newNode->val = arr[i];
+        newNode->next = nullptr;
+        current->next = newNode;
+        current = newNode;
     }
     return head;
 }
 
 // Функция для освобождения памяти списка
-void deleteList(ListNode* head) {
+template <typename T>
+void deleteList(ListNode<T>* head) {
     while (head) {
         ListNode* temp = head;
         head = head->next;
@@ -43,7 +46,6 @@ void deleteList(ListNode* head) {
 // Функция, которая перемещает последний элемент списка после головы
 void moveLastToSecond(ListNode* head) {
     if (!head || !head->next || !head->next->next) {
-        // В списке меньше 3 элементов, ничего делать не нужно
         return;
     }
 
@@ -56,19 +58,13 @@ void moveLastToSecond(ListNode* head) {
     }
     // curr - последний узел
     // prev - предпоследний узел
-
-    // Отделить последний узел
     prev->next = nullptr;
-
-    // Вставить его после головы
     curr->next = head->next;
     head->next = curr;
 }
 
 int main() {
     setlocale(LC_ALL, "Russian");
-    int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-    int size = sizeof(arr) / sizeof(arr[0]);
 
     ListNode* head = createList(arr, size);
 
@@ -80,6 +76,5 @@ int main() {
     cout << "После перемещения последнего элемента после головы: ";
     printList(head);
 
-    deleteList(head);
     return 0;
 }
